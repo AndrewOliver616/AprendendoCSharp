4defa7d21f02cb151fa1aeef86a3cd139e5b8c4b
Andre Oliveira <79090202+AndrewOliver616@users.noreply.github.com>
2021-12-12 15:23:34 -0300

    Atualização com o tema Condicionais

--- 7-Condicionais/Program.cs
+++ 7-Condicionais/Program.cs
@@ -10,6 +10,24 @@ namespace _7_Condicionais
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Executando Projeto 7 Condicionais");
+
+            int idadeJoao = 19;
+
+            if (idadeJoao >= 18)
+            {
+                Console.WriteLine("João é maior de 18 anos, o acesso ao Evento é PERMITIDO!.");
+            }
+
+            else
+            {
+
+                Console.WriteLine("João é menor de 18 anos, o acesso ao Evento é NEGADO!");
+
+            }
+            //O if (SE) é um operador de condição, o else (caso não/contrário) é um operador de condição contrária/oposta ao declado no if.
+
+            Console.ReadLine();
         }
     }
 }
