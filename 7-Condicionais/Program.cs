f7fd9814382ef7c17d3e809f81803c5e4acb2490
Andre Oliveira <79090202+AndrewOliver616@users.noreply.github.com>
2021-12-12 15:58:44 -0300

    adicionando operadores lógicos/boleanos.

--- 7-Condicionais/Program.cs
+++ 7-Condicionais/Program.cs
@@ -12,7 +12,8 @@ namespace _7_Condicionais
         {
             Console.WriteLine("Executando Projeto 7 Condicionais");
 
-            int idadeJoao = 19;
+            int idadeJoao = 16;
+            int acompanhado = 2;
 
             if (idadeJoao >= 18)
             {
@@ -21,12 +22,37 @@ namespace _7_Condicionais
 
             else
             {
-
-                Console.WriteLine("João é menor de 18 anos, o acesso ao Evento é NEGADO!");
+                if (acompanhado >= 2)
+                {
+                    Console.WriteLine("João é menor de 18 anos, mas está acompanhado, o acesso é PERMITIDO!");
+                }
+                else
+                {
+                    Console.WriteLine("João é menor de 18 anos, o acesso ao Evento é NEGADO!");
+                }
 
             }
             //O if (SE) é um operador de condição, o else (caso não/contrário) é um operador de condição contrária/oposta ao declado no if.
 
+            int idadeJose = 15;
+            int acompanhado2 = 2;
+
+            if(idadeJose >= 18)
+            {
+                Console.WriteLine("José tem 18 anos ou mais, Bem-vindo! Pode entrar na festa");
+            }
+            else
+            {
+               if(acompanhado2 >= 2)
+                {
+                    Console.WriteLine("José menor de 18 anos, mas está acompanhado, pode entrar na fest, Diverta-Se!");
+                }
+               else
+                {
+                    Console.WriteLine("José ainda não possui 18 anos ou mais, não pode entrar na festa desacompanhado!");
+                }
+                
+            }
             Console.ReadLine();
         }
     }
---
+++ 8-Condicionais2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Condicionais2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 8 Condicionais2");
+
+            int idadeJoao = 16;
+            int acompanhado = 2;
+
+            //No C# para utilizar a condicionais OU utiliza-se o ||
+            if (idadeJoao >= 18 || acompanhado >= 2)
+            {
+                Console.WriteLine("O acesso ao Evento é PERMITIDO!.");
+            }
+
+            else
+            {
+
+                Console.WriteLine("O acesso ao Evento é NEGADO!");
+            }
+
+            //O if (SE) é um operador de condição, o else (caso não/contrário) é um operador de condição contrária/oposta ao declado no if.
+
+
+
+            int idadeJose = 19;
+            int acompanhado2 = 3;
+            //bool acompanhados = acompanhado2;
+            bool acompanhados = true;
+
+            //0 && representa a conectivo logico E
+            if (idadeJose >= 18 && acompanhados == true)
+            {
+                Console.WriteLine(" Bem-vindo! Pode entrar na festa");
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar na festa desacompanhado!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
