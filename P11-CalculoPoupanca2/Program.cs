1b88c82b503db842e8578d5a01e237dbe373af57
Andre Oliveira <79090202+AndrewOliver616@users.noreply.github.com>
2021-12-13 01:01:20 -0300

    Adicionando laço de repetição FOR encadeado.

---
+++ P11-CalculoPoupanca2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P11_CalculoPoupanca2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 11 Calculo Poupança 2");
+
+            double valorInvestido = 1000;
+
+            for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
+            {
+                valorInvestido = valorInvestido * 1.0036;
+                Console.WriteLine(
+                    "Após " + contadorMes +
+                    " meses, você terá R$" + valorInvestido);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ P12-CalculaInvestimentoLongoPrazo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P12_CalculaInvestimentoLongoPrazo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto P12 Cálculo de Investimento em Longo Prazo.");
+
+            double valorInvestido = 1000;
+            double fatorRendimento = 1.0036;
+
+            //Repetição de laços FOR encadeados
+            for (int contadorAno = 1; contadorAno <= 5; contadorAno++)
+            {
+                for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
+                {
+                    //*= multiplica o valor antes da = pelo indice indicado em seguida.
+                    valorInvestido *= fatorRendimento;
+                }
+                fatorRendimento += 0.0010;
+            }
+
+            Console.WriteLine("Ao termino do investimento você terá R$ " + valorInvestido + " de retorno");
+
+            Console.ReadLine();
+        }
+    }
+}
