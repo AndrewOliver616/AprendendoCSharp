06aab16bbe5cad4330fb8bdcc36eebccf8b1ea68
Andre Oliveira <79090202+AndrewOliver616@users.noreply.github.com>
2021-12-16 10:55:10 -0300

    FOR Encadeado e uso do BREAK

---
+++ P13-ForEncadeado/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P13_ForEncadeado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escrevendo Asteriscos com o BREAK
+            Console.WriteLine("Executando Projeto 13 - FOR Encadeado");
+
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+                {
+                    Console.Write("*");
+                    if (contadorColuna >= contadorLinha)
+                        break;
+
+                }
+
+                Console.WriteLine();
+            }
+
+            //Uma forma diferente de fazer o desenho de asteriscos
+
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+                {
+                    Console.Write("*");
+                    if (contadorColuna >= contadorLinha)
+                        break;
+
+                }
+
+                Console.WriteLine();
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
